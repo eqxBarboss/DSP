23c9cbf10cf073ffd7d0a00e8ca4690dcd718ebe
Daniil Yaskevich <eqxbarboss@gmail.com>
2020-11-25 15:39:35 +0300

    asdasd

--- DSP/DSP.cpp
+++ DSP/DSP.cpp
@@ -38,6 +38,8 @@ float middleOfThree(float a, float b, float c)
 		return c;
 }
 
+
+
 int main()
 {
 	// VARIANT 5
@@ -47,74 +49,59 @@ int main()
 	const float phi = 0;
 
 	std::unique_ptr<TMB1> signal = Generator::GenerateHarmonicSignal(2048, B1, phi, f);
+	//
+	Generator::AllocateBufferAndInitSignalData(*signal, 4);
+	signal->buffer[0] = 4;
+	signal->buffer[1] = 3;
+	signal->buffer[2] = 1;
+	signal->buffer[3] = 6;
+	//
+
+	Generator::SetMinAndMax(*signal);
+	std::stringstream fileNameStream;
+	fileNameStream << std::fixed << std::setprecision(3) << "Signal 1";
+	SaveSignal(fileNameStream.str(), *signal);
+
+	auto signal1 = new TMB1();
+	CopySignal(*signal, signal1);
 
 	for (int i = 0; i < signal->dataSize; i++)
 	{
 		for (int j = 50; j <= 70; j++)
 		{
-			int multiplier = (std::rand() < (RAND_MAX / 2)) ? 1 : -1;
-			signal->buffer[i] += multiplier * B2 * std::sin(2 * PI * i * j / 2048);
+			signal1->buffer[i] = -signal1->buffer[i];
 		}
 	}
 
-	Generator::SetMinAndMax(*signal);
-
-	std::stringstream fileNameStream;
-	fileNameStream << std::fixed << std::setprecision(3) << "Generated signal";
-	SaveSignal(fileNameStream.str(), *signal);
-
-	// Скользящее усреднение с окном 3
-
-	auto signal1 = new TMB1();
-	CopySignal(*signal, signal1);
-
-	for (int i = 1; i < 2046; i++)
-	{
-		signal1->buffer[i] = middleOfThree(signal->buffer[i], signal->buffer[i - 1], signal->buffer[i + 1]);
-	}
+	//
+	Generator::AllocateBufferAndInitSignalData(*signal1, 3);
+	signal1->buffer[0] = 5;
+	signal1->buffer[1] = 2;
+	signal1->buffer[2] = 3;
+	//
 
 	Generator::SetMinAndMax(*signal1);
-
 	std::stringstream fileNameStream1;
-	fileNameStream1 << std::fixed << std::setprecision(3) << "Window 3";
+	fileNameStream1 << std::fixed << std::setprecision(3) << "Signal 2";
 	SaveSignal(fileNameStream1.str(), *signal1);
 
-	// Медианная фильтрация с окном 7
+	auto r = new TMB1();
+	int signalSize = signal->dataSize + signal1->dataSize - 1;
+	Generator::AllocateBufferAndInitSignalData(*r, signalSize);
 
-	auto signal2 = new TMB1();
-	CopySignal(*signal, signal2);
-
-	for (int i = 3; i < 2044; i++)
+	for (int i = 0; i < signalSize; i++)
 	{
-		std::vector<float> vec;
-		for (int j = i - 3; j < i + 4; j++) vec.push_back(signal->buffer[j]);
-		std::sort(vec.begin(), vec.end());
-		float sum = 0;
-		for (int j = i - 2; j < i + 3; j++) sum += signal->buffer[j];
-		sum /= 5;
-		signal2->buffer[i] = sum;
+		for (int k = 0; k <= i; k++)
+		{
+			float val = signal->buffer[(k + i) % signal->dataSize];
+			if (k < signal1->dataSize) val *= signal1->buffer[k];
+			else val = 0;
+			r->buffer[i] += val;
+		}
 	}
 
-	Generator::SetMinAndMax(*signal2);
-
-	std::stringstream fileNameStream2;
-	fileNameStream2 << std::fixed << std::setprecision(3) << "Median 7";
-	SaveSignal(fileNameStream2.str(), *signal2);
-
-	// Парабола 4 степени по 7 точкам
-
-	auto signal3 = new TMB1();
-	CopySignal(*signal, signal3);
-
-	for (int i = 3; i < 2044; i++)
-	{		
-		signal3->buffer[i] = (5*signal->buffer[i -3] - 30 * signal->buffer[i - 2] + 75 * signal->buffer[i - 1] + 131 * signal->buffer[i] + 75 * signal->buffer[i + 1]
-			- 30 * signal->buffer[i + 2] + 5 * signal->buffer[i + 3])/(float)231;
-	}
-
-	Generator::SetMinAndMax(*signal3);
-
-	std::stringstream fileNameStream3;
-	fileNameStream3 << std::fixed << std::setprecision(3) << "Parabola 4, 7 points";
-	SaveSignal(fileNameStream3.str(), *signal3);
+	Generator::SetMinAndMax(*r);
+	std::stringstream fileNameStreamR;
+	fileNameStreamR << std::fixed << std::setprecision(3) << "R";
+	SaveSignal(fileNameStreamR.str(), *r);
 }
